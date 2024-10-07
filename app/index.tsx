311a56c6e4ff73cd213a0504bdc94283b16bc397
thilanRandika <dgtrandika@gmail.com>
2024-10-07 12:05:27 +0530

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import {View , Text } from 'react-native'
+
+const index = () => {
+    return (
+        <View>
+            <Text className="text-xl font-bold text-green-700">index</Text>
+        </View>
+    )
+}
+
+export default index
